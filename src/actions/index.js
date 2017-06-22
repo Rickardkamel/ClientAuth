9415a0abae7a61c6b9c910839f9af749acc46c86
rickardkamel <rickardkamel@gmail.com>
2017-06-22 07:55:42 +0200

    added nested login for signup,  added verifyChar for requests with JWT.

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,8 @@ import { browserHistory } from 'react-router';
 import {
     AUTH_USER,
     UNAUTH_USER,
-    AUTH_ERROR
+    AUTH_ERROR,
+    VALIDATE_USER
 } from './types';
 
 const API_URL = 'http://localhost:8090'
@@ -42,6 +43,22 @@ export function signupUser({ username, password}) {
     }
     return function (dispatch) {
         axios.post(`${API_URL}/register/user`, body)
+            .then(response => {
+                // create form-data object for API
+                // and log user in to get token.
+                let body = new FormData();
+                body.append('user', username);
+                body.append('password', password);
+                axios.post(`${API_URL}/authenticate/user`, body)
+                    .then(response => {
+                        dispatch({ type: AUTH_USER });
+                        localStorage.setItem('token', response.data.token);
+                        browserHistory.push('/feature');
+                    })
+            })
+            .catch(err => {
+                dispatch(authError(err.response.data.error));
+            })
     }
 }
 
@@ -56,4 +73,34 @@ export function signoutUser() {
     localStorage.removeItem('token');
 
     return { type: UNAUTH_USER }
+}
+
+export function verifyCharacter() {
+    let body = {
+        account: "nokka",
+        character: "nokkatrapper"
+    }
+    // TODO: Kolla med Stefan varför vi inte kan
+    // skicka med "Authorization" header.
+    return function (dispatch) {
+        axios.post(`${API_URL}/verify/character`, body, {
+            headers:
+            {
+                Authorization: 'Bearer ' + localStorage.getItem('token')
+            }
+        })
+            .then(response => {
+                console.log(response);
+                dispatch({
+                    type: VALIDATE_USER,
+                    payload: response.data.message
+                });
+            })
+            .catch(err => {
+                console.log(err.response);
+            })
+        /*
+                Authorization:Bearer TKN
+                 */
+    }
 }
